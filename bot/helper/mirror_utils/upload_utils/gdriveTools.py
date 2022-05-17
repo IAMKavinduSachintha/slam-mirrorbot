84c7638db2e44f80cfaa86e4aa46ec5f0b784e68
IAMKavinduSachintha <105691474+IAMKavinduSachintha@users.noreply.github.com>
2022-05-17 09:07:38 +0530

    Update gdriveTools.py

--- bot/helper/mirror_utils/upload_utils/gdriveTools.py
+++ bot/helper/mirror_utils/upload_utils/gdriveTools.py
@@ -190,7 +190,7 @@ class GoogleDriveHelper:
         # File body description
         file_metadata = {
             'name': file_name,
-            'description': 'Uploaded by Slam Mirrorbot',
+            'description': 'Uploaded by JohnDoeLK',
             'mimeType': mime_type,
         }
         try:
